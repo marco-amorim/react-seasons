6df1049ef933dfaf433841e0a492a800e2d4631f
Marco <marcoamorim95@hotmail.com>
2019-10-24 21:56:07 -0300

    course exercises

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO this.state
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // we called setstate !!!!
+                this.setState({ lat: position.coords.latitude });
+
+                //DON'T DO THIS !!!!
+                //this.state.lat = position.coords.latitude
+            }, 
+
+            err => console.log(err)
+        
+        );
+    }
+
+    // React says we have to define render!!!
+    render() {
+        
+        return <div>Latitude: {this.state.lat}</div>
+    }
+}
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
